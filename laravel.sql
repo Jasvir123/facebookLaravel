eb75487b8c3077f6f7ddd96a5384da52865b4ecf
jasvir <jasvir.10000@gmail.com>
2023-07-06 17:56:45 +0530

    update mysql DB

--- laravel.sql
+++ laravel.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jul 06, 2023 at 06:27 AM
+-- Generation Time: Jul 06, 2023 at 02:25 PM
 -- Server version: 10.4.28-MariaDB
 -- PHP Version: 8.2.4
 
@@ -41,11 +41,9 @@ CREATE TABLE `comments` (
 --
 
 INSERT INTO `comments` (`id`, `post_id`, `user_id`, `comment`, `created_at`, `updated_at`) VALUES
-(1, 2, 2, 'Testing', '2023-07-05 12:33:10', '2023-07-05 12:33:10'),
-(2, 1, 2, 'comment2', '2023-07-05 12:47:06', '2023-07-05 12:47:06'),
-(3, 2, 2, 'comment', '2023-07-06 03:59:14', '2023-07-06 03:59:14'),
-(4, 1, 2, 'comment 3', '2023-07-06 03:59:28', '2023-07-06 03:59:28'),
-(5, 2, 3, 'comment4', '2023-07-06 04:00:04', '2023-07-06 04:00:04');
+(6, 4, 10, 'nice post', '2023-07-06 10:17:00', '2023-07-06 10:17:00'),
+(7, 4, 12, 'good!', '2023-07-06 10:17:37', '2023-07-06 10:17:37'),
+(10, 7, 12, 'new one', '2023-07-06 10:25:21', '2023-07-06 10:25:21');
 
 -- --------------------------------------------------------
 
@@ -86,7 +84,42 @@ INSERT INTO `migrations` (`id`, `migration`, `batch`) VALUES
 (4, '2019_12_14_000001_create_personal_access_tokens_table', 1),
 (5, '2023_07_05_064411_create_posts_table', 2),
 (6, '2023_07_05_153305_rename_user_id_column', 3),
-(7, '2023_07_05_164302_create_comments_table', 4);
+(7, '2023_07_05_164302_create_comments_table', 4),
+(8, '2023_07_06_114411_create_permission_tables', 5),
+(9, '2023_07_06_172028_add_columns_to_users', 6);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `model_has_permissions`
+--
+
+CREATE TABLE `model_has_permissions` (
+  `permission_id` bigint(20) UNSIGNED NOT NULL,
+  `model_type` varchar(255) NOT NULL,
+  `model_id` bigint(20) UNSIGNED NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `model_has_roles`
+--
+
+CREATE TABLE `model_has_roles` (
+  `role_id` bigint(20) UNSIGNED NOT NULL,
+  `model_type` varchar(255) NOT NULL,
+  `model_id` bigint(20) UNSIGNED NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `model_has_roles`
+--
+
+INSERT INTO `model_has_roles` (`role_id`, `model_type`, `model_id`) VALUES
+(1, 'App\\Models\\User', 10),
+(1, 'App\\Models\\User', 12),
+(2, 'App\\Models\\User', 11);
 
 -- --------------------------------------------------------
 
@@ -99,6 +132,28 @@ CREATE TABLE `password_reset_tokens` (
   `token` varchar(255) NOT NULL,
   `created_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `permissions`
+--
+
+CREATE TABLE `permissions` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `guard_name` varchar(255) NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `permissions`
+--
+
+INSERT INTO `permissions` (`id`, `name`, `guard_name`, `created_at`, `updated_at`) VALUES
+(1, 'delete post', 'web', '2023-07-06 05:13:35', '2023-07-06 05:13:35'),
+(2, 'delete comment', 'web', '2023-07-06 05:13:35', '2023-07-06 05:13:35');
 
 -- --------------------------------------------------------
 
@@ -140,8 +195,49 @@ CREATE TABLE `posts` (
 --
 
 INSERT INTO `posts` (`id`, `user_id`, `description`, `media`, `visibility`, `created_at`, `updated_at`) VALUES
-(1, 2, 'post1', 'public/images/1688552768.png', 0, '2023-07-05 10:26:08', '2023-07-05 10:26:08'),
-(2, 2, 'post2', 'public/images/1688553529.jpg', 0, '2023-07-05 10:38:49', '2023-07-05 10:38:49');
+(4, 10, 'post2', 'public/images/1688638609.jpg', 0, '2023-07-06 10:16:49', '2023-07-06 10:16:49'),
+(7, 12, 'post4', 'public/images/1688639105.jpg', 0, '2023-07-06 10:25:05', '2023-07-06 10:25:05');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `roles`
+--
+
+CREATE TABLE `roles` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `guard_name` varchar(255) NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `roles`
+--
+
+INSERT INTO `roles` (`id`, `name`, `guard_name`, `created_at`, `updated_at`) VALUES
+(1, 'user', 'web', '2023-07-06 05:13:35', '2023-07-06 05:13:35'),
+(2, 'admin', 'web', '2023-07-06 05:13:35', '2023-07-06 05:13:35');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `role_has_permissions`
+--
+
+CREATE TABLE `role_has_permissions` (
+  `permission_id` bigint(20) UNSIGNED NOT NULL,
+  `role_id` bigint(20) UNSIGNED NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `role_has_permissions`
+--
+
+INSERT INTO `role_has_permissions` (`permission_id`, `role_id`) VALUES
+(1, 2),
+(2, 2);
 
 -- --------------------------------------------------------
 
@@ -157,17 +253,28 @@ CREATE TABLE `users` (
   `password` varchar(255) NOT NULL,
   `remember_token` varchar(100) DEFAULT NULL,
   `created_at` timestamp NULL DEFAULT NULL,
-  `updated_at` timestamp NULL DEFAULT NULL
+  `updated_at` timestamp NULL DEFAULT NULL,
+  `lastName` varchar(255) NOT NULL,
+  `gender` char(20) NOT NULL,
+  `dob` timestamp NULL DEFAULT NULL,
+  `isBlocked` tinyint(1) NOT NULL,
+  `isActive` tinyint(1) NOT NULL,
+  `profileImage` varchar(100) NOT NULL,
+  `address` varchar(510) NOT NULL,
+  `contactNo` bigint(20) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
 -- Dumping data for table `users`
 --
 
-INSERT INTO `users` (`id`, `name`, `email`, `email_verified_at`, `password`, `remember_token`, `created_at`, `updated_at`) VALUES
-(1, 'user1', 'user@email.com', NULL, '$2y$10$nrunNyxs8lW957geeaHwk.8ZFSfhOi.HGmeKwQ6OP7lQK3Wq4zh16', NULL, '2023-07-04 23:32:29', '2023-07-04 23:32:29'),
-(2, 'user1', 'user1@email.com', NULL, '$2y$10$KIpBZ0fYUrzoqQiDzrGXH.zlC2jrhERQGQ4BpSNquu4Hpn75guJJe', NULL, '2023-07-04 23:48:56', '2023-07-04 23:48:56'),
-(3, 'user2', 'user2@email.com', NULL, '$2y$10$.fr9j6oAiHYzTc7VAU46tOQ7GtPCIzMYBahRGlUQj22d4eteJm05K', NULL, '2023-07-06 03:59:52', '2023-07-06 03:59:52');
+INSERT INTO `users` (`id`, `name`, `email`, `email_verified_at`, `password`, `remember_token`, `created_at`, `updated_at`, `lastName`, `gender`, `dob`, `isBlocked`, `isActive`, `profileImage`, `address`, `contactNo`) VALUES
+(2, 'user1', 'user1@email.com', NULL, '$2y$10$KIpBZ0fYUrzoqQiDzrGXH.zlC2jrhERQGQ4BpSNquu4Hpn75guJJe', NULL, '2023-07-04 23:48:56', '2023-07-04 23:48:56', '', '', NULL, 0, 0, '', '', 0),
+(3, 'user2', 'user2@email.com', NULL, '$2y$10$.fr9j6oAiHYzTc7VAU46tOQ7GtPCIzMYBahRGlUQj22d4eteJm05K', NULL, '2023-07-06 03:59:52', '2023-07-06 03:59:52', '', '', NULL, 0, 0, '', '', 0),
+(8, 'user3', 'user3@email.com', NULL, '$2y$10$ObIwZp50i7V8/wpwKfuza.vZMAG.fJYRCIcZrsz7xxNJnVBzvFS/S', NULL, '2023-07-06 05:31:59', '2023-07-06 05:31:59', '', '', NULL, 0, 0, '', '', 0),
+(10, 'user4', 'user4@email.com', NULL, '$2y$10$lsJcwx/6buObEtMCiYVD4u/hRGSw7wKys6iIqsMPtZfKGwsNb9gzC', NULL, '2023-07-06 06:22:08', '2023-07-06 06:22:08', '', '', NULL, 0, 0, '', '', 0),
+(11, 'admin', 'admin@email.com', NULL, '$2y$10$8wjtCM8bkrFW/xKvuvhFEehiAYcMkJrqgsg3o6xsc6/bgLuwQioxu', NULL, '2023-07-06 06:23:18', '2023-07-06 06:23:18', '', '', NULL, 0, 0, '', '', 0),
+(12, 'user5', 'user5@email.com', NULL, '$2y$10$58lBt1EvQ9NHAma02vxoz.ORyBxV8EJRREK2PRyA1iMrWU3aVHPsC', NULL, '2023-07-06 07:55:56', '2023-07-06 07:55:56', '', '', NULL, 0, 0, '', '', 0);
 
 --
 -- Indexes for dumped tables
@@ -195,10 +302,31 @@ ALTER TABLE `migrations`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indexes for table `model_has_permissions`
+--
+ALTER TABLE `model_has_permissions`
+  ADD PRIMARY KEY (`permission_id`,`model_id`,`model_type`),
+  ADD KEY `model_has_permissions_model_id_model_type_index` (`model_id`,`model_type`);
+
+--
+-- Indexes for table `model_has_roles`
+--
+ALTER TABLE `model_has_roles`
+  ADD PRIMARY KEY (`role_id`,`model_id`,`model_type`),
+  ADD KEY `model_has_roles_model_id_model_type_index` (`model_id`,`model_type`);
+
+--
 -- Indexes for table `password_reset_tokens`
 --
 ALTER TABLE `password_reset_tokens`
   ADD PRIMARY KEY (`email`);
+
+--
+-- Indexes for table `permissions`
+--
+ALTER TABLE `permissions`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `permissions_name_guard_name_unique` (`name`,`guard_name`);
 
 --
 -- Indexes for table `personal_access_tokens`
@@ -215,6 +343,20 @@ ALTER TABLE `posts`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indexes for table `roles`
+--
+ALTER TABLE `roles`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `roles_name_guard_name_unique` (`name`,`guard_name`);
+
+--
+-- Indexes for table `role_has_permissions`
+--
+ALTER TABLE `role_has_permissions`
+  ADD PRIMARY KEY (`permission_id`,`role_id`),
+  ADD KEY `role_has_permissions_role_id_foreign` (`role_id`);
+
+--
 -- Indexes for table `users`
 --
 ALTER TABLE `users`
@@ -229,7 +371,7 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `comments`
 --
 ALTER TABLE `comments`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 
 --
 -- AUTO_INCREMENT for table `failed_jobs`
@@ -241,7 +383,13 @@ ALTER TABLE `failed_jobs`
 -- AUTO_INCREMENT for table `migrations`
 --
 ALTER TABLE `migrations`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+
+--
+-- AUTO_INCREMENT for table `permissions`
+--
+ALTER TABLE `permissions`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `personal_access_tokens`
@@ -253,13 +401,19 @@ ALTER TABLE `personal_access_tokens`
 -- AUTO_INCREMENT for table `posts`
 --
 ALTER TABLE `posts`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
+-- AUTO_INCREMENT for table `roles`
+--
+ALTER TABLE `roles`
   MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
 
 --
 -- Constraints for dumped tables
@@ -271,6 +425,25 @@ ALTER TABLE `users`
 ALTER TABLE `comments`
   ADD CONSTRAINT `comments_post_id_foreign` FOREIGN KEY (`post_id`) REFERENCES `posts` (`id`),
   ADD CONSTRAINT `comments_user_id_foreign` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`);
+
+--
+-- Constraints for table `model_has_permissions`
+--
+ALTER TABLE `model_has_permissions`
+  ADD CONSTRAINT `model_has_permissions_permission_id_foreign` FOREIGN KEY (`permission_id`) REFERENCES `permissions` (`id`) ON DELETE CASCADE;
+
+--
+-- Constraints for table `model_has_roles`
+--
+ALTER TABLE `model_has_roles`
+  ADD CONSTRAINT `model_has_roles_role_id_foreign` FOREIGN KEY (`role_id`) REFERENCES `roles` (`id`) ON DELETE CASCADE;
+
+--
+-- Constraints for table `role_has_permissions`
+--
+ALTER TABLE `role_has_permissions`
+  ADD CONSTRAINT `role_has_permissions_permission_id_foreign` FOREIGN KEY (`permission_id`) REFERENCES `permissions` (`id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `role_has_permissions_role_id_foreign` FOREIGN KEY (`role_id`) REFERENCES `roles` (`id`) ON DELETE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
