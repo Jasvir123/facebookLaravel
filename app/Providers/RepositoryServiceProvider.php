f97caed2577063c2ee551f14a067eed2ae4532f3
jasvir <jasvir.10000@gmail.com>
2023-07-07 09:59:40 +0530

    add repository service provider

---
+++ app/Providers/RepositoryServiceProvider.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class RepositoryServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     */
+    public function register(): void
+    {
+        // bind repositories
+    }
+
+    /**
+     * Bootstrap services.
+     */
+    public function boot(): void
+    {
+        //
+    }
+}
+
