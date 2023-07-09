fa6b6d1c62edd00bb3521e6bbd4b02651d653a4d
jasvir <jasvir.10000@gmail.com>
2023-07-09 17:53:21 +0530

    shift profile routes in user.php

--- routes/user.php
+++ routes/user.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\AdminController;
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware('admin')->group(function () {
@@ -10,8 +11,8 @@ Route::middleware('admin')->group(function () {
 
 });
 
-Route::middleware('user')->group(function () {
-    Route::get('/user/dashboard', function () {
-        return "<p>user dashboard</p>";
-    });
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
