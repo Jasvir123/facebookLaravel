8c5deb45f3b15f6f5c2188285de7f3e1e062a581
jasvir <jasvir.10000@gmail.com>
2023-07-11 13:26:51 +0530

    add heading posts

--- resources/views/admin/posts.blade.php
+++ resources/views/admin/posts.blade.php
@@ -1,4 +1,9 @@
 <x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('Posts') }}
+        </h2>
+    </x-slot>
     @push('css')
         <link href="{{ asset('css/adminPosts.css') }}" rel="stylesheet">
     @endpush
